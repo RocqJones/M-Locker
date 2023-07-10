6081edb682852b39fd8d2aedfd7b2509aad03b03
RocqJones <jonesmbindyo@gmail.com>
2023-07-10 16:24:41 +0300

    theme update

--- app/src/main/java/com/rocqjones/mlocker/ui/theme/Color.kt
+++ app/src/main/java/com/rocqjones/mlocker/ui/theme/Color.kt
@@ -2,10 +2,11 @@ package com.rocqjones.mlocker.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val lightBg = Color(0xFFFEFEFF)
+val lightTextColor = Color(0xFF000000)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val darkBg = Color(0xFF414141)
+val darkTextColor = Color(0xFFFFFFFF)
+
+val secondaryColor = Color(0xFFEFB8C8)
+val tertiaryColor = Color(0xFFEFB8C8)
--- app/src/main/java/com/rocqjones/mlocker/ui/theme/Theme.kt
+++ app/src/main/java/com/rocqjones/mlocker/ui/theme/Theme.kt
@@ -16,15 +16,25 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = darkBg,
+    onPrimary = darkTextColor,
+    secondary = secondaryColor,
+    onSecondary = darkTextColor,
+    tertiary = tertiaryColor,
+    surface = darkBg,
+    onSurface = darkTextColor,
+    background = darkBg,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = lightBg,
+    onPrimary = lightTextColor,
+    secondary = secondaryColor,
+    onSecondary = lightTextColor,
+    tertiary = tertiaryColor,
+    surface = lightBg,
+    onSurface = lightTextColor,
+    background = lightBg
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
